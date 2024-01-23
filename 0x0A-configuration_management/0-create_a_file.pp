667a78adc9d63ce346877b6d16701d0270113b87
emmaolondo <emmaolondo448@gmail.com>
2024-01-23 04:38:08 -0800

    create puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # a file that specifies the owner, group, mode  and content using puppet
-file {'/tmp/school':
+file { '/tmp/school':
   ensure  => present,
   mode    => '0774',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
