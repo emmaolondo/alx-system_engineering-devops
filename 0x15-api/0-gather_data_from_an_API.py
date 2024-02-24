248fe929d1f124a792ee827c171348e9fb909215
emmaolondo <emmaolondo448@gmail.com>
2024-02-24 17:51:38 +0300

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,12 +2,12 @@
 """ Python script that, using this REST API, for a given employee ID
 returns information about his/her TODO list progress
 """
-
-
 import requests
 import sys
 
+
 if __name__ == "__main__":
+    """ return information about an employee """
 
     if len(sys.argv) != 2:
         sys.exit(1)
