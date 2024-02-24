4f90fdf14855c018adb004732b2d8302381ca018
emmaolondo <emmaolondo448@gmail.com>
2024-02-24 17:48:40 +0300

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Python script that, using this REST API, for a given employee ID,\
-        returns information about his/her TODO list progress
+""" Python script that, using this REST API, for a given employee ID
+returns information about his/her TODO list progress
 """
 
 
