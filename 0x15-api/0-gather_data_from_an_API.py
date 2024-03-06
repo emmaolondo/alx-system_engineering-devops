17c99a99bf8b96cca09ee5a1d67ab4cd88c97302
emmaolondo <emmaolondo448@gmail.com>
2024-03-06 04:27:42 +0300

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     name = user.json().get('name')
 
     taskCount = 0
-    completed = 0
+    t_completed = 0
+    list_tasks = []
 
     todo = requests.get(todo_url)
     tasks = todo.json()
@@ -27,10 +28,12 @@ if __name__ == "__main__":
         if i.get('userId') == employeeId:
             taskCount += 1
             if i.get('completed'):
-                completed += 1
+                list_tasks.append(i.get('title'))
+                t_completed += 1
 
-    print(f'Employee {name} is done with tasks({completed}/{taskCount}):')
+    print(f'Employee {name} is done with tasks({t_completed}/{taskCount}):')
 
-    for j in tasks:
-        if j.get('userId') == employeeId and j.get('completed'):
-            print(f'\n\t {j['title']}', end="")
+    for tasks in list_tasks:
+        print('\t', tasks)
+        # if j.get('userId') == employeeId and j.get('completed'):
+            #print(f'\n\t {j['title']}', end="")
