613feb0be7c597ac471e8965fe799a6d79daefb0
emmaolondo <emmaolondo448@gmail.com>
2024-03-06 05:05:32 +0300

    PEP 8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,17 @@ def number_of_subscribers(subreddit):
     """
     Function that accesses the REDDIT API
     If not valid return 0
+
+    Args:
+        subreddit (str) : The name of subreddit
+
+    Return:
+        int: The number of subscribers, or 0
     """
-    req = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(subreddit),
-            headers={"User-Agent": "Custom"}
-            )
+    url =  "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Custom"}
+
+    req = requests.get(url, headers=headers)
 
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
