7a6ad4b9c69599fad11f2786d2606e364fa80149
emmaolondo <emmaolondo448@gmail.com>
2024-07-26 12:44:59 +0300

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]");
+		printf("Value checked array[%lu] = [%d]", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
